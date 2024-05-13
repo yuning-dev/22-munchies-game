fd2c25b3cbc17c6ef87bd5b06528c72f801fe21c
yuning-dev <yuning.eng@gmail.com>
2024-05-13 12:58:05 +0100

    add treats to canvas

--- canvas-minigame.js
+++ canvas-minigame.js
@@ -1,9 +1,8 @@
 const canvas = document.getElementById('mainCanvas')
 let c = canvas.getContext('2d')
 
-let circlePosition = {
-    x: 0,
-    y: 0
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
 }
 
 function drawAxis() {
@@ -18,6 +17,12 @@ function drawAxis() {
     c.stroke()
 }
 
+
+let circlePosition = {
+    x: 0,
+    y: 0
+}
+
 function drawCircle() {
     c.reset()
     drawAxis()
@@ -29,25 +34,42 @@ function drawCircle() {
     c.resetTransform()
 }
 
+
+let treats = []
+
 function drawTreats() {
-    c.fillStyle = 'orange'
-    c.fillRect()
+    let treatPosition = {
+        x: 0,
+        y: 0
+    }
+    for (let i = 0; i < 10; i++) {
+        treatPosition.x = getRandomInt(986)
+        treatPosition.y = getRandomInt(736)
+        treats.push(treatPosition)
+        c.fillStyle = 'orange'
+        c.fillRect(treatPosition.x, treatPosition.y, 15, 15)
+    }
 }
 
 drawAxis()
 drawCircle()
+drawTreats()
 
 window.addEventListener('keydown', function(event) {
     if (event.code == 'KeyD') {
         circlePosition.x += 50
+        drawCircle()
     } else if (event.code == 'KeyA') {
         circlePosition.x -= 50
+        drawCircle()
     } else if (event.code === 'KeyS') {
         circlePosition.y += 50
+        drawCircle()
     } else if (event.code === 'KeyW') {
         circlePosition.y -= 50
+        drawCircle()
     }
-    drawCircle()
+
 })
 
 
