1600738b7a61aab71b2ba65749c212f68f152ed0
yuning-dev <yuning.eng@gmail.com>
2024-05-13 12:32:56 +0100

    add circle which moves to user input

--- canvas-minigame.js
+++ canvas-minigame.js
@@ -1,35 +1,53 @@
 const canvas = document.getElementById('mainCanvas')
 let c = canvas.getContext('2d')
 
-c.fillStyle = 'lightblue'
-c.fillRect(200, 200, 100, 100)
-c.moveTo(0, 0)
-c.lineTo(1000, 750)
-c.stroke()
-
-c.beginPath()
-c.arc(500, 375, 50, 0, 2 * Math.PI)
-c.fillStyle = 'pink'
-c.fill()
-
-c.beginPath()
-c.moveTo(500, 375)
-c.lineTo(750, 562.5)
-c.lineTo(250, 562.5)
-c.strokeStyle = 'purple'
-c.lineWidth = 5
-c.closePath()
-c.stroke()
-
-c.fillStyle = 'lightgreen'
-for(let i = 0; i < 4; i++) {
-    c.translate(100, 0)
-    c.fillRect(100, 100, 50, 200)
+let circlePosition = {
+    x: 0,
+    y: 0
 }
 
-c.strokeStyle = 'crimson'
-c.lineWidth = 3
-c.translate(-200, 500)
-let rads = 200 * 2 * Math.PI / 360
-c.rotate(rads)
-c.strokeRect(0, 0, 150, 150)
+function drawAxis() {
+    c.beginPath()
+    c.moveTo(500, 0)
+    c.lineTo(500, 750)
+    c.stroke()
+
+    c.beginPath()
+    c.moveTo(0, 375)
+    c.lineTo(5000, 375)
+    c.stroke()
+}
+
+function drawCircle() {
+    c.reset()
+    drawAxis()
+    c.beginPath()
+    c.translate(circlePosition.x, circlePosition.y)
+    c.arc(500, 375, 50, 0, 2 * Math.PI)
+    c.fillStyle = 'lightblue'
+    c.fill()
+    c.resetTransform()
+}
+
+function drawTreats() {
+    c.fillStyle = 'orange'
+    c.fillRect()
+}
+
+drawAxis()
+drawCircle()
+
+window.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyD') {
+        circlePosition.x += 50
+    } else if (event.code == 'KeyA') {
+        circlePosition.x -= 50
+    } else if (event.code === 'KeyS') {
+        circlePosition.y += 50
+    } else if (event.code === 'KeyW') {
+        circlePosition.y -= 50
+    }
+    drawCircle()
+})
+
+
